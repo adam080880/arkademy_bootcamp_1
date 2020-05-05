2904f040502bb227b8603c2ee75901fc26a7fe3b
Muhamad Adam 84 <muhamadadam20@gmail.com>
2020-05-05 15:50:37 +0700

    fix

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ Object.keys(identity).forEach((val, index) => {
 })
 
 // Username and password check
-let username = "mrzombie80"
+let username = "username"
 let password = "password"
 
-console.log('\n\n')
+console.log('\n')
 console.log('Username:', username)
 console.log('Password:', password)
-console.log('Pass validate:', validate(username, password))
+console.log('Account validation passed:', validate(username, password))
